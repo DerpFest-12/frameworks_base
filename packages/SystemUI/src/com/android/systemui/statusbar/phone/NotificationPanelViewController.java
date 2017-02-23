e089d821ba7fbf43de8fccc57bf4d9c2e945dc51
maxwen <max.weninger@gmail.com>
2017-02-24 01:28:00 +0300

    SystemUI: config to hide status bar on lock screen [1/2]
    visibility can be toggled with tapping at the status bar
    
    AICPfied: brought up to O, 11 & 12 by @eyosen
    
    BenzoEdit: Fixed system icons for Pie
    Change-Id: Ib05b4960f47bd65fb40122507cb7ea3d48b4d436

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -4766,6 +4766,9 @@ public class NotificationPanelViewController extends PanelViewController {
                     mKeyguardStatusBarViewController.updateViewState(
                             /* alpha= */ 1f,
                             keyguardShowing ? View.VISIBLE : View.INVISIBLE);
+                if (keyguardShowing) {
+                    mKeyguardStatusBar.toggleContents(true);
+                }
                 }
                 if (keyguardShowing && oldState != mBarState) {
                     if (mQs != null) {
