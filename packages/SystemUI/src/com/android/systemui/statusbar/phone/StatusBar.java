ccc7f79fbb932799eba6b270824f3c28195f135e
ezio84 <brabus84@gmail.com>
2017-09-06 23:59:02 +0200

    Allow to disable qs footer warnings [1/2]
    When using apps like AdGuard that create a vpn, a warning is displayed
    in the QS panel footer, so the user knows the device net is being monitored
    by an external app.
    The warning takes space and the user might want to avoid it if he already
    knows that the app is running (also because there's a statusbar icon
    for the same purpose)
    
    Change-Id: I909cacc67aa44f62489068d3903b7a24e94ea18a
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3919,6 +3919,9 @@ public class StatusBar extends SystemUI implements
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.SHOW_LOCKSCREEN_MEDIA_ART),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_FOOTER_WARNINGS),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -3942,6 +3945,9 @@ public class StatusBar extends SystemUI implements
             }  else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.SHOW_LOCKSCREEN_MEDIA_ART))) {
                 setLockScreenMediaArt();
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_FOOTER_WARNINGS))) {
+                setQsPanelOptions();
             }
         }
 
@@ -3950,7 +3956,8 @@ public class StatusBar extends SystemUI implements
             setLockscreenDoubleTapToSleep();
             setStatusBarWindowViewOptions();
             setLockScreenMediaBlurLevel();
-            setLockScreenMediaArt();         
+            setLockScreenMediaArt();
+            setQsPanelOptions();
         }
     }
 
@@ -3971,6 +3978,12 @@ public class StatusBar extends SystemUI implements
     private void setStatusBarWindowViewOptions() {
         if (mNotificationShadeWindowViewController != null) {
             mNotificationShadeWindowViewController.setStatusBarWindowViewOptions();
+        }
+    }
+
+    private void setQsPanelOptions() {
+        if (mQSPanelController != null) {
+            mQSPanelController.updateSettings();
         }
     }
 
