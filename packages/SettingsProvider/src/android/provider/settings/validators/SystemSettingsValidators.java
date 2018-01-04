628c9ab3b8dda690eee604f4eb68f0ca187c4af1
ezio84 <brabus84@gmail.com>
2018-01-04 10:26:17 +0100

    Allow to suppress notifications sound/vibration if screen is ON [1/2]
    Change-Id: I279b202682939d797d3116089f50d65e3dd3eb01

--- packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
@@ -257,5 +257,6 @@ public class SystemSettingsValidators {
         VALIDATORS.put(System.FLASHLIGHT_ON_CALL_IGNORE_DND, BOOLEAN_VALIDATOR);
         VALIDATORS.put(System.FLASHLIGHT_ON_CALL_RATE, new InclusiveIntegerRangeValidator(1, 5));
         VALIDATORS.put(System.VOLUME_DIALOG_TIMEOUT, new InclusiveIntegerRangeValidator(1, 7));
+        VALIDATORS.put(System.NOTIFICATION_SOUND_VIB_SCREEN_ON, BOOLEAN_VALIDATOR);
     }
 }
