2eaf8a7159647b60bf431b3bc8371e449adb5e21
maxwen <max.weninger@gmail.com>
2018-08-16 20:53:46 +0200

    SystemUI: Add navigation bar visibility control
    setting navbar_visibility to force visibility
    independent of the config
    
    [spezi77] Removed dependency to org.omnirom.omnilib.utils.DeviceUtils
    
    Change-Id: Iabbf75f4432fab449e233794b6a65f4cdc2f2af5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6269,6 +6269,11 @@ public final class Settings {
         public static final String AMBIENT_BATTERY_PERCENT = "ambient_battery_percent";
 
         /**
+         * @hide
+         */
+        public static final String OMNI_NAVIGATION_BAR_SHOW = "navbar_visibility";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -6454,6 +6459,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(MUSIC_TILE_TITLE);
             PRIVATE_SETTINGS.add(OMNI_KEYGUARD_SHOW_BATTERY_BAR);
             PRIVATE_SETTINGS.add(OMNI_KEYGUARD_SHOW_BATTERY_BAR_ALWAYS);
+            PRIVATE_SETTINGS.add(OMNI_NAVIGATION_BAR_SHOW);
             PRIVATE_SETTINGS.add(AMBIENT_BATTERY_PERCENT);
         }
 
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -124,6 +124,7 @@ import com.android.internal.logging.UiEventLoggerImpl;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.statusbar.RegisterStatusBarResult;
+import com.android.internal.util.derp.derpUtils;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
 import com.android.keyguard.ViewMediatorCallback;
@@ -529,6 +530,10 @@ public class StatusBar extends SystemUI implements
 
     protected GameSpaceManager mGameSpaceManager;
     protected TaskHelper mTaskHelper;
+
+    /*omni add-on*/
+    private SettingsObserver mSettingsObserver;
+    private boolean mShowNavBar;
 
     // expanded notifications
     // the sliding/resizing panel within the notification window
@@ -967,6 +972,8 @@ public class StatusBar extends SystemUI implements
         mDisplayId = mDisplay.getDisplayId();
         updateDisplaySize();
         mStatusBarHideIconsForBouncerManager.setDisplayId(mDisplayId);
+        mSettingsObserver = new SettingsObserver(mMainHandler);
+        mSettingsObserver.observe();
 
         // start old BaseStatusBar.start().
         mWindowManagerService = WindowManagerGlobal.getWindowManagerService();
@@ -1208,7 +1215,7 @@ public class StatusBar extends SystemUI implements
         mHeadsUpManager.addListener(mVisualStabilityManager);
         mNotificationPanelViewController.setHeadsUpManager(mHeadsUpManager);
 
-        createNavigationBar(result);
+        updateNavigationBar(true);
 
         if (ENABLE_LOCKSCREEN_WALLPAPER && mWallpaperSupported) {
             mLockscreenWallpaper = mLockscreenWallpaperLazy.get();
@@ -1992,6 +1999,46 @@ public class StatusBar extends SystemUI implements
         KeyboardShortcutsMessage(int deviceId) {
             mDeviceId = deviceId;
         }
+    }
+
+    private class SettingsObserver extends ContentObserver {
+
+        SettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.OMNI_NAVIGATION_BAR_SHOW),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.OMNI_NAVIGATION_BAR_SHOW))) {
+                updateNavigationBar(false);
+            }
+        }
+    }
+
+    private void updateNavigationBar(boolean init) {
+        boolean showNavBar = derpUtils.deviceSupportNavigationBar(mContext);
+        if (init) {
+            if (showNavBar) {
+                mNavigationBarController.createNavigationBars(true, null);
+            }
+        } else {
+            if (showNavBar != mShowNavBar) {
+                if (showNavBar) {
+                    mNavigationBarController.createNavigationBars(true, null);
+                } else {
+                    mNavigationBarController.removeNavigationBar(mDisplayId);
+                }
+            }
+        }
+        mShowNavBar = showNavBar;
     }
 
     static class AnimateExpandSettingsPanelMessage {
