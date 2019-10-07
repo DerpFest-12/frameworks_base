65a4645a9a178ccae2667853e615456bedf29ab1
darkobas <darkobas@gmail.com>
2019-10-07 20:31:48 +0200

    base: add option to enable AOD on charging only [1/2]
    @idoybh edits: Adapted to A11's settings backup
    @jhonboy121: adapt to A12 changes in DozeParameters. Also in the og commit DOZE_ALWAYS_ON was being read from System namespace instead of Secure, fixed it here. Make use of SettingsProxy utlity classes for settings / getting values
    
    Change-Id: I831583fde68de15788e3d7ecab55d864726d140f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6009,6 +6009,18 @@ public final class Settings {
         public static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
 
         /**
+         * Whether to enable DOZE only when charging
+         * @hide
+         */
+        public static final String DOZE_ON_CHARGE = "doze_on_charge";
+
+        /**
+         * Hidden setting of the current atate of DOZE only when charging
+         * @hide
+         */
+        public static final String DOZE_ON_CHARGE_NOW = "doze_on_charge_now";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
