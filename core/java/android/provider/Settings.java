773e71d0f7b4df182d2c8f64ba2aad887759e8cd
maxwen <max.weninger@gmail.com>
2019-11-27 16:27:40 +0100

    SystemUI: bring back old style mobile data indicators
    TODO maybe add settings observer to do update faster then
    waiting for the next update
    
    Change-Id: I3367bf0b7ff5b6483efc820a86dec8469af6e034
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5924,6 +5924,11 @@ public final class Settings {
         public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
 
         /**
+         * @hide
+         */
+        public static final String USE_OLD_MOBILETYPE = "use_old_mobiletype";  
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
