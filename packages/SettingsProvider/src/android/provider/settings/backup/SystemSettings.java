6130c084c735429df511149c7c5374333e0d8841
Pranav Vashi <neobuddy89@gmail.com>
2019-12-25 08:40:02 +0300

    Disable Notifications vibration [1/2]
    Change-Id: I4337f6d7bfb84d19d38fea9f33e196d8bc47e228

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -106,5 +106,6 @@ public class SystemSettings {
         Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL,
         Settings.System.SHOW_FOURG_ICON,
         Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN,
+        Settings.System.VIBRATE_ON_NOTIFICATIONS,
     };
 }
