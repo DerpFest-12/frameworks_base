eeda7e7b726bedc02e7f42d2fe73fead6efa949e
TikkiTikki <radstylemusic@gmail.com>
2021-02-02 01:19:07 -0300

    base: Show and edit Ambient Icons on Lockscreen [1/2]
    Change-Id: Idde03d4f937ffe8da3b56ccec0ee91a3dc7bbd1e
    [nurkeinneid] adapt for android 12L
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationIconAreaController.java
@@ -2,7 +2,9 @@ package com.android.systemui.statusbar.phone;
 
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
@@ -271,12 +273,27 @@ public class NotificationIconAreaController implements
                 mIconSize + 2 * mIconHPadding, mStatusBarWindowController.getStatusBarHeight());
     }
 
+    @NonNull
+    private FrameLayout.LayoutParams generateAodIconLayoutParams() {
+        return new FrameLayout.LayoutParams(getAodIconsSize(), getAodIconsSize());
+    }
+
     private void reloadDimens(Context context) {
         Resources res = context.getResources();
         mIconSize = res.getDimensionPixelSize(com.android.internal.R.dimen.status_bar_icon_size);
         mIconHPadding = res.getDimensionPixelSize(R.dimen.status_bar_icon_padding);
         mAodIconAppearTranslation = res.getDimensionPixelSize(
                 R.dimen.shelf_appear_translation);
+    }
+
+    private int getAodIconsSize() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.AMBIENT_ICONS_SIZE, 80, UserHandle.USER_CURRENT);
+    }
+
+    private int getAodIconsColor() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.AMBIENT_ICONS_COLOR, Color.WHITE, UserHandle.USER_CURRENT);
     }
 
     /**
@@ -538,6 +555,8 @@ public class NotificationIconAreaController implements
         }
 
         final FrameLayout.LayoutParams params = generateIconLayoutParams();
+        final FrameLayout.LayoutParams aodParams = generateAodIconLayoutParams();
+        final boolean isAodHostLayout = hostLayout == mAodIcons;
         for (int i = 0; i < toShow.size(); i++) {
             StatusBarIconView v = toShow.get(i);
             // The view might still be transiently added if it was just removed and added again
@@ -546,7 +565,7 @@ public class NotificationIconAreaController implements
                 if (hideDismissed) {
                     v.setOnDismissListener(mUpdateStatusBarIcons);
                 }
-                hostLayout.addView(v, i, params);
+                hostLayout.addView(v, i, isAodHostLayout ? aodParams : params);
             }
             v.setIconStyle(mNewIconStyle);
             v.setShowCount(mShowNotificationCount);
@@ -564,7 +583,7 @@ public class NotificationIconAreaController implements
                 continue;
             }
             hostLayout.removeView(expected);
-            hostLayout.addView(expected, i);
+            hostLayout.addView(expected, i, isAodHostLayout ? aodParams : params);
         }
         hostLayout.setChangingViewPositions(false);
         hostLayout.setReplacingIcons(null);
@@ -699,8 +718,7 @@ public class NotificationIconAreaController implements
     }
 
     private void reloadAodColor() {
-        mAodIconTint = Utils.getColorAttrDefaultColor(mContext,
-                R.attr.wallpaperTextColor);
+        mAodIconTint = getAodIconsColor();
     }
 
     private void updateAodIconColors() {
@@ -740,8 +758,12 @@ public class NotificationIconAreaController implements
         if (mAodIcons == null) {
             return;
         }
+        boolean showIconsLockScreen = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.AMBIENT_ICONS_LOCKSCREEN,
+                0, UserHandle.USER_CURRENT) != 0;
         boolean visible = mBypassController.getBypassEnabled()
-                || mWakeUpCoordinator.getNotificationsFullyHidden();
+                || mWakeUpCoordinator.getNotificationsFullyHidden() || showIconsLockScreen;
+        mAodIcons.getLayoutParams().height = getAodIconsSize();
 
         // Hide the AOD icons if we're not in the KEYGUARD state unless the screen off animation is
         // playing, in which case we want them to be visible since we're animating in the AOD UI and
@@ -750,7 +772,7 @@ public class NotificationIconAreaController implements
                 && !mUnlockedScreenOffAnimationController.isScreenOffAnimationPlaying()) {
             visible = false;
         }
-        if (visible && mWakeUpCoordinator.isPulseExpanding()
+        if (visible && mWakeUpCoordinator.isPulseExpanding() && !showIconsLockScreen
                 && !mBypassController.getBypassEnabled()) {
             visible = false;
         }
