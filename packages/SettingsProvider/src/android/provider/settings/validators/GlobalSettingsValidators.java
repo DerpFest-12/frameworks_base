06710c7f3e957e7089418fe77ec503348388d3f6
Oliver Scott <olivercscott@gmail.com>
2021-02-25 11:29:35 -0500

    Wi-Fi timeout feature
    Add Wi-Fi timeout setting
    
    Issue: calyxos#228
    Change-Id: I1ac6b19eb35af656afd38eac24bb37fe618859f1
    (cherry picked from commit 12a44815c0d1c64c2684782a87707675181e19e6)

--- packages/SettingsProvider/src/android/provider/settings/validators/GlobalSettingsValidators.java
+++ packages/SettingsProvider/src/android/provider/settings/validators/GlobalSettingsValidators.java
@@ -145,6 +145,21 @@ public class GlobalSettingsValidators {
         VALIDATORS.put(Global.ADVANCED_BATTERY_USAGE_AMOUNT, PERCENTAGE_INTEGER_VALIDATOR);
         VALIDATORS.put(Global.POWER_BUTTON_LONG_PRESS_DURATION_MS, NONE_NEGATIVE_LONG_VALIDATOR);
         VALIDATORS.put(Global.WAKE_WHEN_PLUGGED_OR_UNPLUGGED, BOOLEAN_VALIDATOR);
+        VALIDATORS.put(Global.WIFI_OFF_TIMEOUT,
+                new DiscreteValueValidator(new String[] {
+                        "0",
+                        "15000",
+                        "30000",
+                        "60000",
+                        "120000",
+                        "300000",
+                        "600000",
+                        "1800000",
+                        "3600000",
+                        "7200000",
+                        "14400000",
+                        "28800000"
+                }));
     }
 }
 
