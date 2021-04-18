642aa8a01a59ea684f5da08c490fcc76654e4c9e
TikkiTikki <radstylemusic@gmail.com>
2021-04-18 19:37:06 -0300

    Pulse: Vertical mirror [1/2]

--- packages/SystemUI/src/com/android/systemui/pulse/SolidLineRenderer.java
+++ packages/SystemUI/src/com/android/systemui/pulse/SolidLineRenderer.java
@@ -57,6 +57,7 @@ public class SolidLineRenderer extends Renderer {
 
     private boolean mSmoothingEnabled;
     private boolean mCenterMirrored;
+    private boolean mVerticalMirror;
     private boolean mRounded;
     private CMRendererObserver mObserver;
 
@@ -257,7 +258,16 @@ public class SolidLineRenderer extends Renderer {
 
     @Override
     public void draw(Canvas canvas) {
+        canvas.scale(1, 1, mWidth / 2f, mHeight / 2f);
         canvas.drawLines(mFFTPoints, mPaint);
+        if (mVerticalMirror) {
+            if (mVertical) {
+                canvas.scale(-1, 1, mWidth / 2f, mHeight / 2f);
+            } else {
+                canvas.scale(1, -1, mWidth / 2f, mHeight / 2f);
+            }
+            canvas.drawLines(mFFTPoints, mPaint);
+        }
     }
 
     @Override
@@ -310,6 +320,10 @@ public class SolidLineRenderer extends Renderer {
             resolver.registerContentObserver(
                     Settings.Secure.getUriFor(Settings.Secure.PULSE_SOLID_UNITS_ROUNDED), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(
+                    Settings.Secure.getUriFor(Settings.Secure.PULSE_VERTICAL_MIRROR), false,
+                    this,
+                    UserHandle.USER_ALL);
         }
 
         @Override
@@ -328,6 +342,8 @@ public class SolidLineRenderer extends Renderer {
                     Settings.Secure.PULSE_SMOOTHING_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
             mCenterMirrored = Settings.System.getIntForUser(resolver,
                     Settings.System.VISUALIZER_CENTER_MIRRORED, 0, UserHandle.USER_CURRENT) == 1;
+            mVerticalMirror = Settings.Secure.getIntForUser(resolver,
+                    Settings.Secure.PULSE_VERTICAL_MIRROR, 0, UserHandle.USER_CURRENT) == 1;
             mGravity = Settings.Secure.getIntForUser(
                     resolver, Settings.Secure.PULSE_CUSTOM_GRAVITY, 0,
                     UserHandle.USER_CURRENT);
