68ee80282de8859433572e33b05b4a4084e1a98a
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:57:38 +0530

    SystemUI: update monet according to new sysui stub
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- packages/SystemUI/src/org/derpfest/systemui/theme/ThemeOverlayControllerDerpFest.kt
+++ packages/SystemUI/src/org/derpfest/systemui/theme/ThemeOverlayControllerDerpFest.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.protonaosp.systemui.theme
+package org.derpfest.systemui.theme
 
 import android.annotation.ColorInt
 import android.app.WallpaperColors
@@ -53,7 +53,7 @@ import kotlin.math.log10
 import kotlin.math.pow
 
 @SysUISingleton
-class CustomThemeOverlayController @Inject constructor(
+class ThemeOverlayControllerDerpFest @Inject constructor(
     private val context: Context,
     broadcastDispatcher: BroadcastDispatcher,
     @Background bgHandler: Handler,
@@ -165,7 +165,8 @@ class CustomThemeOverlayController @Inject constructor(
     }
 
     companion object {
-        private const val TAG = "CustomThemeOverlayController"
+        private const val TAG = "ThemeOverlayControllerDerpFest"
+        private const val DEBUG = false
 
         private const val PREF_PREFIX = "monet_engine"
         private const val PREF_COLOR_OVERRIDE = "${PREF_PREFIX}_color_override"
