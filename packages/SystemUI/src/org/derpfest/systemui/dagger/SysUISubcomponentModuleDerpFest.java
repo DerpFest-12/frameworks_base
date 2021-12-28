8e28fb2ce84dd452181cdbdd478c961266e54a8e
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:49:21 +0530

    SystemUI: Initial DerpFest SystemUI stub
    Change-Id: I7aa765a5803d3e7a6de1ba16335eaf3f10faecc8
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/5675
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>

---
+++ packages/SystemUI/src/org/derpfest/systemui/dagger/SysUISubcomponentModuleDerpFest.java
@@ -0,0 +1,10 @@
+package org.derpfest.systemui.dagger;
+
+import dagger.Module;
+
+/**
+ * Dagger module for including the WMComponent.
+ */
+@Module(subcomponents = {SysUIComponentDerpFest.class})
+public abstract class SysUISubcomponentModuleDerpFest {
+}
