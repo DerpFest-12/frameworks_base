4ee8228b353d6df79ba62f9d551d7336a2039032
jhonboy121 <alfredmathew05@gmail.com>
2022-01-19 11:16:48 +0530

    SystemUI: allow toggling haptic feedback on pressing udfps view [1/2]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6350,6 +6350,13 @@ public final class Settings {
         public static final String ENABLE_FLOATING_ROTATION_BUTTON = "enable_floating_rotation_button";
 
         /**
+         * Whether to play haptic feedback on udfps finger down event.
+         * Default 1
+         * @hide
+         */
+        public static final String ENABLE_UDFPS_START_HAPTIC_FEEDBACK = "enable_udfps_start_haptic_feedback";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
