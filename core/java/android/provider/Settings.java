2bc099d5dd9e624bd70f3edd7ead1f5796d1c38d
Ali B <abittin@gmail.com>
2022-02-06 14:10:07 +0300

    Settings: make SensorBlock readable by non-system apps
    Change-Id: Iaf7bb605c0741e14a3845ee73241f02d6d293cad

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6382,6 +6382,7 @@ public final class Settings {
          * Sensor block per-package
          * @hide
          */
+        @Readable
         public static final String SENSOR_BLOCK = "sensor_block";
 
         /**
