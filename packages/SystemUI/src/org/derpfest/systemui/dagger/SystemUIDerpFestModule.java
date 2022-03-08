087499d4c79c9cb3a31356ddfcaed0d4fc1af1b6
Anay Wadhera <anay1018@gmail.com>
2022-03-08 10:07:41 +0000

    DerpFestSystemUI: catch up to 12L SystemUIDefaultModule
    Change-Id: Ib316311e2eef30a7dbc296a30397b143bbf35aab

--- packages/SystemUI/src/org/derpfest/systemui/dagger/SystemUIDerpFestModule.java
+++ packages/SystemUI/src/org/derpfest/systemui/dagger/SystemUIDerpFestModule.java
@@ -37,8 +37,7 @@ import com.android.systemui.plugins.qs.QSFactory;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
 import com.android.systemui.power.EnhancedEstimates;
 import com.android.systemui.power.EnhancedEstimatesImpl;
-import com.android.systemui.power.PowerUI;
-import com.android.systemui.power.PowerNotificationWarnings;
+import com.android.systemui.power.dagger.PowerModule;
 import com.android.systemui.qs.dagger.QSModule;
 import com.android.systemui.qs.tileimpl.QSFactoryImpl;
 import com.android.systemui.recents.Recents;
@@ -82,8 +81,9 @@ import javax.inject.Named;
 
 @Module(includes = {
         MediaModule.class,
+        PowerModule.class,
         QSModule.class,
-        VolumeModule.class  
+        VolumeModule.class
 })
 public abstract class SystemUIDerpFestModule {
 
@@ -173,12 +173,6 @@ public abstract class SystemUIDerpFestModule {
             ConfigurationController configurationController) {
         return new HeadsUpManagerPhone(context, statusBarStateController, bypassController,
                 groupManager, configurationController);
-    }
-
-    @SysUISingleton
-    @Provides
-    static PowerUI.WarningsUI provideWarningsUi(PowerNotificationWarnings controllerImpl) {
-        return controllerImpl;
     }
 
     @Binds
