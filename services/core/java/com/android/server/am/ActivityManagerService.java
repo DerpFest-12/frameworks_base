d6e22a05f7dcb21632f50fa7db47f77b9c68ea30
LibXZR <i@xzr.moe>
2022-03-08 22:31:44 +0800

    s2ss: Remove property usage
    It really make no sense to use dirty SystemProperties inside an
    expensive IPC. It simply acts as a global variable, so why?
    
    Remove expensive lock and SystemProperties usage to optimize for
    performance because it is called under hot path.
    
    Change-Id: I9512e6bed50be8f808f5cca93efe6424f5d327df
    Signed-off-by: LibXZR <i@xzr.moe>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1515,6 +1515,7 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     final SwipeToScreenshotObserver mSwipeToScreenshotObserver;
     private boolean mIsSwipeToScreenshotEnabled;
+    private boolean mIsSwipeToScreenshotActive;
 
     /**
      * Used to notify activity lifecycle events.
@@ -17346,9 +17347,12 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     @Override
     public boolean isSwipeToScreenshotGestureActive() {
-        synchronized (this) {
-            return mIsSwipeToScreenshotEnabled && SystemProperties.getBoolean("sys.android.screenshot", false);
-        }
+        return mIsSwipeToScreenshotEnabled && mIsSwipeToScreenshotActive;
+    }
+
+    @Override
+    public void setSwipeToScreenshotGestureActive(boolean enabled) {
+        mIsSwipeToScreenshotActive = enabled;
     }
 
     @Override
