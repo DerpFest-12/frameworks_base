8b22506c293304b8da1c371165dea51edf942b95
cjh1249131356 <cjh1249131356@gmail.com>
2022-03-15 13:46:38 +0800

    base: Always trigger fullscreen type screenshot for three-fingers-swipe gesture
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1781,6 +1781,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         mSwipeToScreenshot = new SwipeToScreenshotListener(context, new SwipeToScreenshotListener.Callbacks() {
             @Override
             public void onSwipeThreeFinger() {
+                mScreenshotRunnable.setScreenshotType(TAKE_SCREENSHOT_FULLSCREEN);
                 mHandler.post(mScreenshotRunnable);
             }
         });
