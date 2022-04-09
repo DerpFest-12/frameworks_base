f32f01aa99dd3aad75ed258400371ea4ca781bc9
jhonboy121 <alfredmathew05@gmail.com>
2022-04-09 16:10:19 +0200

    base: Allow toggling screen off FOD [1/2]
    idoybh (YAAP): Change default to enabled to retain AOSP behaviour
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -147,5 +147,6 @@ public class SystemSettings {
         Settings.System.SENSOR_BLOCK,
         Settings.System.LOCK_HIDE_STATUS_BAR,
         Settings.System.HEADS_UP_NOTIFICATIONS_THRESHOLD,
+        Settings.System.SCREEN_OFF_FOD,
     };
 }
