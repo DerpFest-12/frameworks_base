CalyxOS/platform_frameworks_base

fixup! SystemUI: Add support for timeout-reboot
Change-Id: I0e4ea7a66e842a49256f3d066696ce7e682556af

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1172,7 +1172,7 @@ public class KeyguardViewMediator extends SystemUI implements Dumpable,
         final ContentResolver cr = mContext.getContentResolver();
 
         // From Global Settings
-        final long timeout = LineageSettings.Global.getInt(cr,
+        final long timeout = LineageSettings.Global.getLong(cr,
                 LineageSettings.Global.DEVICE_REBOOT_TIMEOUT,
                 KEYGUARD_REBOOT_AFTER_DELAY_DEFAULT);
 

DerpFest-12/frameworks_base

packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1182,7 +1182,7 @@ public class KeyguardViewMediator extends SystemUI implements Dumpable,
         final ContentResolver cr = mContext.getContentResolver();
 
         // From Global Settings
-        final long timeout = Settings.Global.getInt(cr,
+        final long timeout = Settings.Global.getLong(cr,
                 Settings.Global.DEVICE_REBOOT_TIMEOUT,
                 KEYGUARD_REBOOT_AFTER_DELAY_DEFAULT);
 