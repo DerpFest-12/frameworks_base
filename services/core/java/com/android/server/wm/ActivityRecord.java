281c6c3733d13b1fe9606c8ebd34c9aa29b6c302
Justin Dunlap <justindunlap@google.com>
2023-03-01 00:07:21 +0000

    Revert "Make Activites touch opaque - DO NOT MERGE"
    This reverts commit 22261fa6649f6ec6441646743ad98132fcf47fe0.
    
    Reason for revert: Re-release due to functional regression
    
    Change-Id: I9ca1fa2f140d640159fabec1424c52867cf01a60
    (cherry picked from commit 23bf0bda7d9b97a82ea04257318bb90677561476)
    Merged-In: I9ca1fa2f140d640159fabec1424c52867cf01a60

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -802,13 +802,6 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
 
     private AppSaturationInfo mLastAppSaturationInfo;
 
-    private final ActivityRecordInputSink mActivityRecordInputSink;
-
-    // Activities with this uid are allowed to not create an input sink while being in the same
-    // task and directly above this ActivityRecord. This field is updated whenever a new activity
-    // is launched from this ActivityRecord. Touches are always allowed within the same uid.
-    int mAllowedTouchUid;
-
     private final ColorDisplayService.ColorTransformController mColorTransformController =
             (matrix, translation) -> mWmService.mH.post(() -> {
                 synchronized (mWmService.mGlobalLock) {
@@ -1854,8 +1847,6 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
             createTime = _createTime;
         }
         mAtmService.mPackageConfigPersister.updateConfigIfNeeded(this, mUserId, packageName);
-
-        mActivityRecordInputSink = new ActivityRecordInputSink(this, sourceRecord);
     }
 
     /**
@@ -3769,7 +3760,6 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
         } else {
             onRemovedFromDisplay();
         }
-        mActivityRecordInputSink.releaseSurfaceControl();
         super.removeImmediately();
     }
 
@@ -6933,9 +6923,6 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
                 getSyncTransaction().show(mSurfaceControl);
             } else if (!show && mLastSurfaceShowing) {
                 getSyncTransaction().hide(mSurfaceControl);
-            }
-            if (show) {
-                mActivityRecordInputSink.applyChangesToSurfaceIfChanged(getSyncTransaction());
             }
         }
         if (mThumbnail != null) {
